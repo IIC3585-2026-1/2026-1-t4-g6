7d07ddd965b089fa98389d39dc11721a3c574ab9
Patricio Cortez <patricio.cortez@uc.cl>
2026-05-09 20:07:21 -0400

    feat: Initialize app controller and worker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
-window.onload = () => {
-  "use strict";
+import { MainController } from "./controller/mainController";
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const app = new MainController();
+  
+  try {
+    await app.init();
+    console.log("Aplicación iniciada correctamente");
+  } catch (err) {
+    console.error("Error al inicar la app:", err);
+  }
 
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./sw.js");
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js");
+      console.log("Service Worker registrado con éxito. Scope:", registration.scope);
+    } catch (err) {
+      console.error("Falló ell registro del Service Worker:", err);
+    }
   }
-};
+});
